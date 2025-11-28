ad160e2b60a1226e80bf25b2a67af26c1cf0c118
Mariam-Aji <mariamaaji442@gmail.com>
2025-11-28 12:56:44 +0300

    editing.

--- Application/Services/AuthService.cs
+++ Application/Services/AuthService.cs
@@ -200,7 +200,7 @@ public class AuthService : IAuthService
             issuer: _config["Jwt:Issuer"],
             audience: _config["Jwt:Audience"],
             claims: claims,
-            expires: DateTime.UtcNow.AddDays(7),
+            expires: DateTime.UtcNow.AddHours(1),
             signingCredentials: creds
         );
         return new JwtSecurityTokenHandler().WriteToken(token);
